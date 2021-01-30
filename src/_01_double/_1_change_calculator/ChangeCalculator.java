ad5d5d224ff2c94127892ed7a52f2629a3dea96f
root <root@359d87c6d416>
2021-01-30 20:55:20 +0000

    Level 0 Module 4 Just Started

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,17 +12,19 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String nickelsString = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+		int nickels = Integer.parseInt(nickelsString);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String dimesString = JOptionPane.showInputDialog("How many dimes do you have?");
+		int dimes = Integer.parseInt(dimesString);
 		// Ask the user how many quarters they have, and convert their answer
-
+		String quartersString = JOptionPane.showInputDialog("How many quarters do you have?");
+		int quarters = Integer.parseInt(quartersString);
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+		double money = (nickels*0.05) + (dimes*0.10) + (quarters*0.25);
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
-
+		JOptionPane.showMessageDialog(null, "You have $" + money);
 	}
 }
 
---
+++ src/_01_double/_2_test_scores/DoubleTestScores.java
@@ -0,0 +1,11 @@
+package _01_double._2_test_scores;
+
+public class DoubleTestScores {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
